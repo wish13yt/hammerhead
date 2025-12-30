fd5a2d5fdc68e274f39b93694caf3c58700e9f49
Wish <wishyt@proton.me>
2025-12-29 20:26:05 -0600

    move to hammerhead repo

---
+++ module/README.md
@@ -0,0 +1,10 @@
+# Hammerhead
+Hammerhead is a module for Python designed to interact with the Sharkey API.
+# Notice
+Hammerhead is not created by or endorsed by the Sharkey team! Hammerhead licensed under GPL-3.0. Hammerhead is under alpha, and may not work at times. Thank you :)
+# Credits
+Thank you:
+
+Sharkey creator people; for being really cool and making the thingy im making a python module for
+
+hacks.guide (nomaakip); for creating a Sharkey server, the way I found out about Sharkey, and being really peak
---
+++ testings/app.py
@@ -0,0 +1,19 @@
+import requests
+print("Please note: This is designed for Sharkey, a fork of Misskey. Some functions may work with Misskey, but support is not provided for Misskey.")
+print("please enter your Sharkey instance without a / at the end, starting with https://")
+sharkeyint = input()
+url = sharkeyint + '/api/notes/create'
+print("great! The API URL I made for you is: " + url)
+print("if this is incorrect, please try again")
+print("enter an API token you obtained by logging into the bot account and going to: " + sharkeyint + "/settings/connect")
+bearer = input()
+header = {'Authorization': 'Bearer ' + bearer}
+print("what do you want to send? this will be visible publicily !!!")
+sendthis = input()
+null = None
+payload = {"text": sendthis,"poll": null,"cw": null,"localOnly": False,"visibility": "public","reactionAcceptance": "nonSensitiveOnly"}
+x = requests.post(url, headers=header, json=payload)
+if x.status_code == 200:
+    print("success with response: " + x.text)
+else:
+    print("ow there was error")
