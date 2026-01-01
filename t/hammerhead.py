c4e5bedfc1061ed6b3a2e3b51ff463d6685feb16
Wish <wishyt@proton.me>
2026-01-01 16:44:55 -0600

    switch to classes alos DJDCHWFKWKHEWKFKH

--- t/hammerhead.py
+++ t/hammerhead.py
@@ -1,22 +1,24 @@
 import requests
-def sign_in(key, url):
-    global apiurl
-    global apikey
-    apiurl = url
-    apikey = key
-    if apiurl.startswith("https://"):
+class signin:
+    def __init__(self, key, url):
+        global apiurl
+        global apikey
         apiurl = url
-    else:
-        apiurl = "https://" + url
-    return apiurl, apikey
-def create_post(post_content):
-    apiurl, apikey = sign_in()
-    if apiurl.endswith("/"):
-        posturl = apiurl + "api/notes/create"
-    else:
-        posturl = apiurl + "/api/notes/create"
-    null = None
-    header = {'Authorization': 'Bearer ' + apikey}
-    payload = {"text": post_content,"poll": null,"cw": null,"localOnly": False,"visibility": "public","reactionAcceptance": "nonSensitiveOnly"}
-    x = requests.post(posturl, headers=header, json=payload)
-    return x.status_code
+        apikey = key
+        if not apiurl.startswith("https://"):
+            apiurl = "https://" + url
+        print(apiurl + " " + apikey)
+class createpost:
+    def __init__(self, post_content):
+            print(signin.apiurl)
+            apiurl = signin.apiurl
+            apikey = signin.apikey
+            if apiurl.endswith("/"):
+                posturl = apiurl + "api/notes/create"
+            else:
+                posturl = apiurl + "/api/notes/create"
+            null = None
+            header = {'Authorization': 'Bearer ' + apikey}
+            payload = {"text": post_content,"poll": null,"cw": null,"localOnly": False,"visibility": "public","reactionAcceptance": "nonSensitiveOnly"}
+            rep = requests.post(posturl, headers=header, json=payload)
+            return rep.status_code
--- t/newapp.py
+++ t/newapp.py
@@ -1,3 +1,3 @@
-from hammerhead import sign_in, create_post
-sign_in(key="poOVzJZq67CnNldLlRZRTVdCHiF9hTbe", url="https://sharkey.nomaakip.xyz")
-create_post(post_content="bunny bunny bunny bunny")
+from hammerhead import signin, createpost
+signin(key="poOVzJZq67CnNldLlRZRTVdCHiF9hTbe", url="https://sharkey.nomaakip.xyz")
+createpost(post_content="bunny bunny bunny bunny")
